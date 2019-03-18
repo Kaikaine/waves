b7bf0e3d279e702e87260eedd9d88472589d59a4
kaikaine <kairijb@protonmail.com>
2019-03-18 05:00:39 -0500

    created user model

--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+// MODELS
+const { User } = require("./models/User");
+
 mongoose.Promise = global.Promise;
 mongoose
   .connect(process.env.DATABASE)
@@ -14,6 +17,23 @@ mongoose
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.unsubscribe(cookieparser());
+
+// USERS
+app.post("/api/users/register", (req, res) => {
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password,
+    name: req.body.name,
+    lastname: req.body.lastname
+  });
+
+  user.save((err, doc) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    res.status(200).json({ success: true, userdata: doc });
+  });
+});
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
